8f74d7bfcefbd8457f46869aa705b156762e616d
Ben Darnell <ben@bendarnell.com>
2012-02-28 15:55:37 -0800

    Circle packing

--- plop/static/plop.js
+++ plop/static/plop.js
@@ -1,56 +1,75 @@
 // based on http://mbostock.github.com/d3/ex/bubble.html
 startDrawing = function(data) {
-    var r = 1600;
-    var treemap = d3.layout.treemap()
-        .size([r,r])
-        .sticky(true)
+    var r = 1000;
+    function calls_value(d) {
+        if (d.weights)
+            return d.weights.calls;
+        else
+            return d3.sum(d.values, calls_value);
+    }
+    function time_value(d) {
+        if (d.weights)
+            return d.weights.time;
+        else
+            return d3.sum(d.values, time_value);
+    }
+    var pack = d3.layout.pack()
+        .size([r, r])
         .children(function(d) { return d.values })
-        .value(function(d) { return d.weights.calls });
+        .value(calls_value);
 
-    var div = d3.select("#treemap");
+    var svg = d3.select("#graph");
     var fill = d3.scale.category20c();
 
     var hier_data = d3.nest()
         .key(function(d) { return d.attrs.filename })
         .entries(data.nodes);
+    hier_data = [{'key': 'root', 'values': hier_data}];
     console.log(hier_data);
 
-    div.data([{'key': 'root', 'values':hier_data}]).selectAll("div")
-        .data(treemap.nodes)
-        .enter().append("div")
-        .attr("class", "cell")
-        .style("background", function (d) { return d.values ? fill(d.key) : null })
-        .text(function(d) { return d.values ? null : d.attrs.funcname })
-        .attr("title", function(d) { return d.values ? null : (d.attrs.filename  + ":" + d.attrs.lineno + ":" + d.attrs.funcname)})
+    function cell() {
+        this.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+        this.select("circle")
+            .attr("r", function(d) { return d.r });
+        this.select("text")
+            .text(function(d) { return d.attrs.funcname.substring(0, d.r/3)});
+    }
+    
+    var node = svg.data(hier_data).selectAll("g.node")
+        .data(pack.nodes)
+        .enter().append("g")
+        .attr("class", "node")
         .call(cell);
+    
+    node.append("title")
+        .text(function(d) { return d.values ? null : (d.attrs.filename  + ":" + d.attrs.lineno + ":" + d.attrs.funcname)});
+
+    node.append("circle")
+        .attr("r", function(d) { return d.r })
+        .attr("opacity", function(d) { if (d.key=='root') return 0.1; return d.values ? 0.2 : 0.6})
+        .attr("fill", function(d) { return d.key ? fill(d.key) : fill(d.attrs.filename)});
+
+    node.filter(function(d) { return !d.values }).append("text")
+        .attr("text-anchor", "middle")
+        .attr("dy", ".3em")
+        .text(function(d) { return d.attrs.funcname.substring(0, d.r/3)});
 
     d3.select("#calls").on("click", function() {
-        div.selectAll("div")
-            .data(treemap.value(function(d) { return d.weights.calls }))
+        svg.selectAll("g.node")
+            .data(pack.value(calls_value))
             .transition()
             .duration(1000)
             .call(cell);
-                 
     });
     d3.select("#time").on("click", function() {
-        div.selectAll("div")
-            .data(treemap.value(function(d) { return d.weights.time }))
+        svg.selectAll("g.node")
+            .data(pack.value(time_value))
             .transition()
             .duration(1000)
             .call(cell);
-                 
+        
     });
 
-    function cell() {
-        this.style("left", function(d) { return d.x + "px" })
-            .style("top", function(d) { return d.y + "px" })
-            .style("width", function(d) { return Math.max(0, d.dx-1) + "px"})
-            .style("height", function(d) { return Math.max(0, d.dy-1) + "px"})
-            .style("position", "absolute")
-            .style("overflow", "hidden")
-            .style("border", "solid 1px white")
-        ;
-    }
 }
 
 fetchData = function() {
