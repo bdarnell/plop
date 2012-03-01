dfc1ad77bc31b5b5f5e241c8037e7cf3921d6b63
Ben Darnell <ben@bendarnell.com>
2012-03-01 12:53:00 -0800

    Fix test runner for python 2.5 and 2.6

--- plop/test/runtests.py
+++ plop/test/runtests.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
+import sys
 import unittest
 
-TEST_MODULES = [
-    'plop.test.callgraph_test',
+COMMON_TESTS = [
     'plop.test.collector_test',
     'plop.test.platform_test',
+    ]
+
+VIEWER_TESTS = [
+    'plop.test.callgraph_test',
     'plop.test.pstats_loader_test',
     ]
+
+# Viewer currently requires python 2.7 (for collections.Counter, could
+# probably be fixed); collector supports >= 2.5.
+if sys.version_info[:2] >= (2,7):
+    TEST_MODULES = COMMON_TESTS + VIEWER_TESTS
+else:
+    TEST_MODULES = COMMON_TESTS
 
 def all():
     return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
