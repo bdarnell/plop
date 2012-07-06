896daf6a6f44bc9d7cf21cb81061d425a4a368da
Ben Darnell <ben@bendarnell.com>
2012-07-06 16:07:41 -0700

    setup.py updates

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(
     name="plop",
+    version="0.1",
     packages=["plop", "plop.test"],
     package_data={
         "plop": [
@@ -10,5 +11,7 @@ setup(
             "static/force.js",
             "static/third_party/d3/d3.v2.js",
             ],
-        }
+        },
+    author="Ben Darnell",
+    url="https://github.com/bdarnell/plop",
     )
