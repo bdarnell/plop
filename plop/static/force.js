3a744de5d987cea18b99940d45ed2dfefe0dc75a
bernii <bkobos@gmail.com>
2014-07-10 16:09:24 -0700

    add super basic styling to get rid of margins
    proper html indenting and new d3.js version
    
    using camelCase, zoom feature, some cleanup
    
    remove d3 v2, update manifest and setup.py

--- plop/static/force.js
+++ plop/static/force.js
@@ -1,63 +1,90 @@
 // based on http://mbostock.github.com/d3/ex/bubble.html
-startDrawing = function(data) {
-    var r = 3200;
-    var svg = d3.select("#graph");
-    svg.attr("width", r).attr("height", r);
-    function node_weight(d) {
-        return d.weights.calls ? Math.log(d.weights.calls) : 1;
-    }
+(function () {
+  'use strict';
+
+  var svg,
+    svgRoot,
+    width = window.innerWidth,
+    height = window.innerHeight;
+
+  var zoomListener = d3.behavior.zoom()
+    .scaleExtent([0.1, 3])
+    .on("zoom", function(){
+      svg.attr("transform", ["translate(", d3.event.translate, ")scale(", d3.event.scale, ")"].join(""));
+    });
+
+  var nodeWeight = function(d) {
+    return d.weights.calls ? Math.log(d.weights.calls) : 1;
+  }
+
+  var updateWindow = function(){
+    var x = window.innerWidth || document.documentElement.clientWidth;
+    var y = window.innerHeight|| document.documentElement.clientHeight;
+
+    svgRoot.attr("width", x).attr("height", y);
+  }
+
+  var dataReady = function(data) {
+    svgRoot = d3.select("#graph");
+    svg = svgRoot.append("g");
+    zoomListener(svgRoot);
+    updateWindow();
+    startDrawing(data);
+  };
+
+  var startDrawing = function(data){
     console.log("creating force");
     var force = d3.layout.force()
-        .charge(function(d) { return -500 * node_weight(d) })
-        .linkDistance(function(d) { return 25 * node_weight(d) })
-        .size([r,r])
-        .nodes(data.nodes)
-        .links(data.edges)
-        .start();
+      .charge(function(d) { return -500 * nodeWeight(d) })
+      .linkDistance(function(d) { return 25 * nodeWeight(d) })
+      .size([width, height])
+      .nodes(data.nodes)
+      .links(data.edges)
+      .start();
     console.log("started force");
     var fill = d3.scale.category20c();
 
-
     var gnodes = svg.selectAll("g.node")
-        .data(data.nodes)
-        .enter().append("g")
-        .attr("class", "node")
-        .call(force.drag);
+      .data(data.nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag);
     gnodes.append("title")
-        .text(function(d) { return d.attrs.filename + ":" + d.attrs.lineno + ":" + d.attrs.funcname + ": " + d.weights.calls });
+      .text(function(d) { return d.attrs.filename + ":" + d.attrs.lineno + ":" + d.attrs.funcname + ": " + d.weights.calls });
     var circles = gnodes.append("circle")
-        .attr("r", function(d) { return 20 * node_weight(d) })
-        .attr("fill", function(d) {return fill(d.attrs.filename) });
+      .attr("r", function(d) { return 20 * nodeWeight(d) })
+      .attr("fill", function(d) {return fill(d.attrs.filename) });
     var texts = gnodes.append("text")
-        .text(function(d) { return d.attrs.funcname })
-        .attr("text-anchor", "middle")
-        .attr("dy", ".3em")
-        .attr("fill", "#000");
+      .text(function(d) { return d.attrs.funcname })
+      .attr("text-anchor", "middle")
+      .attr("dy", ".3em")
+      .attr("fill", "#000");
     console.log("added nodes");
 
     var links = svg.selectAll("line")
-        .data(data.edges)
-        .enter().append("line")
-        .style("stroke-width", function(d) { return Math.max(1, Math.log(d.weights.calls)) })
-        .style("stroke", "#777")
-        .style("stroke-opacity", 0.4)
-        .attr("marker-end", "url(#Triangle)");
+      .data(data.edges)
+      .enter().append("line")
+      .style("stroke-width", function(d) { return Math.max(1, Math.log(d.weights.calls)) })
+      .style("stroke", "#777")
+      .style("stroke-opacity", 0.4)
+      .attr("marker-end", "url(#Triangle)");
     console.log("added links");
 
     force.on("tick", function() {
-        console.log("tick");
-        circles.attr("cx", function(d) { return d.x })
-            .attr("cy", function(d) { return d.y });
-        texts.attr("x", function(d) { return d.x })
-            .attr("y", function(d) { return d.y });
-        links.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-        //force.stop();
+      circles.attr("cx", function(d) { return d.x })
+        .attr("cy", function(d) { return d.y });
+      texts.attr("x", function(d) { return d.x })
+        .attr("y", function(d) { return d.y });
+      links.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
     });
-}
+  }
 
-fetchData = function(filename) {
-    d3.json('/data?filename=' + filename, startDrawing);
-}
+  window.fetchData = function(filename) {
+    d3.json('/data?filename=' + filename, dataReady);
+  }
+  window.onresize = updateWindow;
+
+})();
