68d5b8c1d0c059f2173362e17886dbfeb297ee97
Andreas Fleig <andreas@informatik.uni-mannheim.de>
2015-03-23 22:24:17 +0100

    Refactor collector output into formatter classes
    * Just store stacks during collection phase, process them afterwards
    * Add --max-stacks option for plop output
    * Change file extension to .flame for Flamegraph output

--- plop/collector.py
+++ plop/collector.py
@@ -16,10 +16,9 @@ class Collector(object):
         'real': (platform.ITIMER_REAL, signal.SIGALRM),
     }
 
-    def __init__(self, interval=0.01, mode='virtual', flamegraph=False):
+    def __init__(self, interval=0.01, mode='virtual'):
         self.interval = interval
         self.mode = mode
-        self.flamegraph = flamegraph
         assert mode in Collector.MODES
         timer, sig = Collector.MODES[self.mode]
         signal.signal(sig, self.handler)
@@ -27,8 +26,6 @@ class Collector(object):
         self.reset()
 
     def reset(self):
-        # defaultdict instead of counter for pre-2.7 compatibility
-        self.stack_counts = collections.defaultdict(int)
         self.stacks = list()
         self.samples_remaining = 0
         self.stopping = False
@@ -68,16 +65,64 @@ class Collector(object):
                 code = frame.f_code
                 frames.append((code.co_filename, code.co_firstlineno, code.co_name))
                 frame = frame.f_back
-            if self.flamegraph:
-                self.stacks.append(frames)
-            else:
-                self.stack_counts[tuple(frames)] += 1
+            self.stacks.append(frames)
         end = time.time()
         self.samples_taken += 1
         self.sample_time += (end - start)
 
-    def filter(self, max_stacks):
-        self.stack_counts = dict(sorted(self.stack_counts.iteritems(), key=lambda kv: -kv[1])[:max_stacks])
+
+class CollectorFormatter(object):
+    """
+    Abstract class for output formats
+    """
+    def format(self, collector):
+        raise Exception("not implemented")
+
+    def store(self, collector, filename):
+        with open(filename, "w") as f:
+            f.write(self.format(collector))
+
+
+class PlopFormatter(CollectorFormatter):
+    """
+    Formats stack frames for plop.viewer
+    """
+    def __init__(self, max_stacks=50):
+        self.max_stacks = 50
+
+    def format(self, collector):
+        # defaultdict instead of counter for pre-2.7 compatibility
+        stack_counts = collections.defaultdict(int)
+        for frames in collector.stacks:
+            stack_counts[tuple(frames)] += 1
+        stack_counts = dict(sorted(stack_counts.iteritems(),
+                                key=lambda kv: -kv[1])[:self.max_stacks])
+        return repr(dict(stack_counts))
+
+
+class FlamegraphFormatter(CollectorFormatter):
+    """
+    Creates Flamegraph files
+    """
+    def format(self, collector):
+        output = ""
+        previous = None
+        previous_count = 1
+        for stack in collector.stacks:
+            current = self.format_flame(stack)
+            if current == previous:
+                previous_count += 1
+            else:
+                output += "%s %d\n" % (previous, previous_count)
+                previous_count = 1
+                previous = current
+        output += "%s %d\n" % (previous, previous_count)
+        return output
+
+    def format_flame(self, stack):
+        stack.reverse()
+        funcs = map(lambda stack: "%s (%s:%s)" % (stack[2], stack[0], stack[1]), stack)
+        return ";".join(funcs)
 
 
 def main():
@@ -85,28 +130,42 @@ def main():
     # between tornado.autoreload and auto2to3
     parser = argparse.ArgumentParser(description="Plop: Python Low-Overhead Profiler",
                                      prog="python -m plop.collector")
-    parser.add_argument("--flamegraph", "-f", help="Record stack traces in Flamegraph format",
-                        action="store_const", const=True, default=False)
+    parser.add_argument("--format", "-f", help="Output format. Default: plop",
+                        choices=["plop", "flamegraph"], default="plop")
     parser.add_argument("--module", "-m", help="Execute target as a module",
                         action="store_const", const=True, default=False)
     parser.add_argument("--mode", help=("Interval timer mode to use, see `man 2 setitimer`. "
                         "Default: prof"), choices=["prof", "real", "virtual"], default="prof")
     parser.add_argument("--interval", help="Timer interval in seconds. Default: 0.01",
                         default=0.01, type=float)
-    parser.add_argument("--duration", help="profiling duration in seconds. Default: 3600",
+    parser.add_argument("--duration", help="Profiling duration in seconds. Default: 3600",
                         default=3600, type=int)
+    parser.add_argument("--max-stacks", help=("Number of most frequent stacks to store."
+                        " Ignored for Flamegraph output. Default: 50"), type=int,
+                        default=50)
     parser.add_argument("target", help="Module or script to run")
     parser.add_argument("arguments", nargs=argparse.REMAINDER,
                         help="Pass-through arguments for the profiled application")
     args = parser.parse_args()
     sys.argv = [args.target] + args.arguments
 
+    if args.format == "flamegraph":
+        extension = "flame"
+        formatter = FlamegraphFormatter()
+    elif args.format == "plop":
+        extension = "plop"
+        formatter = PlopFormatter(max_stacks=args.max_stacks)
+    else:
+        sys.stderr.write("Unhandled output format: %s" % args.format)
+        sys.stderr.flush()
+        sys.exit(1)
+
     if not os.path.exists('profiles'):
         os.mkdir('profiles')
-    filename = 'profiles/%s-%s.plop' % (args.target,
-                                        time.strftime('%Y%m%d-%H%M-%S'))
+    filename = 'profiles/%s-%s.%s' % (args.target, time.strftime('%Y%m%d-%H%M-%S'),
+                                      extension)
 
-    collector = Collector(flamegraph=args.flamegraph, mode=args.mode, interval=args.interval)
+    collector = Collector(mode=args.mode, interval=args.interval)
     collector.start(duration=args.duration)
     exit_code = 0
     try:
@@ -124,13 +183,8 @@ def main():
     except SystemExit, e:
         exit_code = e.code
     collector.stop()
-    collector.filter(50)
     if collector.samples_taken:
-        if args.flamegraph:
-            store_flamegraph(filename, collector)
-        else:
-            with open(filename, 'w') as f:
-                f.write(repr(dict(collector.stack_counts)))
+        formatter.store(collector, filename)
         print "profile output saved to %s" % filename
         overhead = float(collector.sample_time) / collector.samples_taken
         print "overhead was %s per sample (%s%%)" % (
@@ -138,27 +192,6 @@ def main():
     else:
         print "no samples collected; program was too fast"
     sys.exit(exit_code)
-
-
-def format_flame(stack):
-    stack.reverse()
-    funcs = map(lambda stack: "%s (%s:%s)" % (stack[2], stack[0], stack[1]), stack)
-    return ";".join(funcs)
-
-
-def store_flamegraph(filename, collector):
-    with open(filename, 'w') as f:
-        previous = None
-        previous_count = 1
-        for stack in collector.stacks:
-            current = format_flame(stack)
-            if current == previous:
-                previous_count += 1
-            else:
-                f.write("%s %d\n" % (previous, previous_count))
-                previous_count = 1
-                previous = current
-        f.write("%s %d\n" % (previous, previous_count))
 
 
 if __name__ == '__main__':
